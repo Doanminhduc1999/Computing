3eae3ef6cec7ff2da152a5f30460ec614bb749a6
Doanminhduc1999 <73933398+Doanminhduc1999@users.noreply.github.com>
2020-11-06 16:08:40 +0700

    Add files via upload

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -1,13 +1,13 @@
 module.exports = {
     localdb: {
-        urldb : 'mongodb+srv://RDfYkRnmj0GbTr16:KSEwLJGtq1Nvya90@cluster0.phozs.mongodb.net/shopbig?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv://RDfYkRnmj0GbTr16:RDfYkRnmj0GbTr16@cluster0.phozs.mongodb.net/<dbname>?retryWrites=true&w=majority',
         dbname : "shopbig",
         dbusername : "",
         dbpassword : "",
     },
 
     clouddb: {
-        urldb : 'mongodb+srv://RDfYkRnmj0GbTr16:KSEwLJGtq1Nvya90@cluster0.phozs.mongodb.net/shopbig?retryWrites=true&w=majority',
+        urldb : 'mongodb+srv://RDfYkRnmj0GbTr16:RDfYkRnmj0GbTr16@cluster0.phozs.mongodb.net/<dbname>?retryWrites=true&w=majority',
         dbname : "shopbig",
         dbusername : "",
         dbpassword : "",
